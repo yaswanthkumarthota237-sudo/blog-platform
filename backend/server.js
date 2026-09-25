c96c6cfaf5bccd67259957be9d08b936f45ac5c1
yaswanthkumarthota237-sudo <yaswanthkumarthota237@gmail.com>
2026-09-25 14:21:18 +0000

    Enable open CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
